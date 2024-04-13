49ae25a3d80c6200b4b3f67e8262941ac76da8ce
Semyon Kachur <57587302+SemyonKachur@users.noreply.github.com>
2024-04-13 10:36:21 +0700

    Добавление AssetContext добавление атрибута для инъекции зависимостей

---
+++ Assets/Scripts/Core/Mobs/SelectableUnit.cs
@@ -0,0 +1,17 @@
+namespace Core.Mobs
+{
+    using Abstractions;
+    using UnityEngine;
+
+    public class SelectableUnit : MonoBehaviour, ISelectable
+    {
+        public float Health => health;
+        [SerializeField, Min(0)] protected float health = 100;
+        
+        public float MaxHealth => maxHealth;
+        [SerializeField,Min(1)] protected float maxHealth = 100;
+
+        public Sprite Icon => icon;
+        [SerializeField] protected Sprite icon = default;
+    }
+}
