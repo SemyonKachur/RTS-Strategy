c13bb7c5a5d78482308ee737eaeddaaa04889092
Semyon Kachur <57587302+SemyonKachur@users.noreply.github.com>
2024-05-01 13:50:49 +0700

    добавление кнопки паузы выхода из игры и возврата в игру

---
+++ Assets/Scripts/UserControlSystem/UI/Presenter/PauseButtonPresenter.cs
@@ -0,0 +1,21 @@
+﻿namespace UserControlSystem.UI.Presenter
+{
+    using UniRx;
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    /// <summary>
+    /// Кнопка открытия меню
+    /// </summary>
+    public class PauseButtonPresenter : MonoBehaviour
+    {
+        [SerializeField]
+        private Button _button = default;
+        [SerializeField]
+        private GameObject _menuPanel = default;
+
+        private void OnEnable() => _button.OnClickAsObservable().Subscribe(OpenMenuPanel);
+
+        private void OpenMenuPanel(Unit unit) => _menuPanel.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/UserControlSystem/UI/Presenter/PausePanelPresenter.cs
@@ -0,0 +1,37 @@
+﻿namespace UserControlSystem.UI.Presenter
+{
+    using System;
+    using UniRx;
+    using UnityEditor;
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    /// <summary>
+    /// Презентер меню паузы.
+    /// </summary>
+    public class PausePanelPresenter : MonoBehaviour
+    {
+        [SerializeField]
+        private Button _backButton = default;
+        [SerializeField]
+        private Button _quitButton = default;
+        [SerializeField]
+        protected GameObject _pausePanel = default;
+        
+        private void OnEnable()
+        {
+            _backButton.OnClickAsObservable().Subscribe(DisablePausePanel);
+            _quitButton.OnClickAsObservable().Subscribe(Quit);
+        }
+
+        private void Quit(Unit unit)
+        {
+#if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+#endif
+            Application.Quit();
+            
+        } 
+        private void DisablePausePanel(Unit unit) => _pausePanel.SetActive(false);
+    }
+}
